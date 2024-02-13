6643348af36b4671a27824fec18bd9c58ebe43a2
motorniko <albertolagos59@gmail.com>
2024-02-13 01:53:01 -0300

    actualizando cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 
 import './App.css';
+import osirislogo from './imagenes/osiris.png';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-cont'>
+        <img
+          className='logo' 
+          src={osirislogo}
+          alt='logo'
+        />
+      </div>
     </div>
   );
 }
